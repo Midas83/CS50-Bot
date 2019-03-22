1af6cf9e6e48b3ec145827dc0bc184f67dc5e661
Midas83 <dexterweatherly@gmail.com>
2019-03-22 16:18:38 -0400

    Added help and resources commands

--- bot.py
+++ bot.py
@@ -1,22 +1,62 @@
 # Discord Bot for CS50
 
 import discord
+print(discord.__version__)
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
 
+        # Intro message whenever bot comes online
+        channel = self.get_channel(416576586301702144)
+        print(channel)
+
+        await channel.send('CS50-Bot is online!.  I\'m here to here ' +
+                              'to help with all your needs while progressing' +
+                              ' through Harvard\'s online CS50 course.  Type ' +
+                              '\"!help\" to learn more about me.')
+    
     async def on_message(self, message):
-        # don't respond to ourselves
+        command = message.content.split()[0]
+        content = message.content.split()
+        del content[0]
+
+        # Don't respond to ourselves
         if message.author == self.user:
             return
-
-        if message.content == '!ping':
+        
+        # Simple test command
+        if command == '!ping':
             await message.channel.send('pong')
 
-        # Search cs50 stack exchange
-        if message.content == '!stack':
-            print(message)
+        # Help command
+        if command == '!help':
+            await message.channel.send(
+                'Here are a list of commands: \n \n' + 
+                '- **!help:**  List of all available commands \n' + 
+                '- **!stack50 <search term>:** Returns results from the CS50 ' +
+                    'Stackexchange \n') 
 
+        # Provide a list of useful links and resources
+        if command == '!resources':
+            await message.channel.send(
+                'The following are some resources you may find helpful ' +
+                'during the course of schoolwork and problemsets: ' +
+                '- **Discord:** https://discord.gg/QYZQfZ6'
+            )
+
+        # Search cs50 stack exchange
+        if command == "!stack50":
+            print(content)
+            # Make sure user provided a search string
+            if content == False:
+                await message.channel.send('Please provide a search term')
+            
+            query = "+"
+            query = query.join(content)
+
+            await message.channel.send('https://cs50.stackexchange.com/search' +
+            '?q=' + query)
+            
 client = MyClient()
 client.run('NTU4MzMyNzQyNjEwMzIxNDA4.D3VTvA.0A4yDcW9cPFExPb1Oq3b4pu4aSs')
